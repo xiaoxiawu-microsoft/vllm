bdbc694a43155e19c69865d9fd3e11417b8c8a00
Your Name <you@example.com>
2024-06-05 14:59:58 +0000

    init push

---
+++ vllm/model_executor/layers/fused_moe/ampere_fp8_fused_moe_large_tokens.py
@@ -0,0 +1,124 @@
+"""Fused MoE kernel."""
+
+import functools
+import json
+import os
+from typing import Any, Dict, Optional, Tuple, Callable
+
+import torch
+import triton
+import triton.language as tl
+
+from vllm import _custom_ops as ops
+from vllm.logger import init_logger
+from vllm.utils import is_hip
+
+logger = init_logger(__name__)
+
+
+from torch.utils.cpp_extension import load
+
+ampere_fp8 = load(
+    name="ampere_fp8",
+    sources=[
+        os.path.join(
+            os.path.dirname(__file__), "csrc", "moe_align_block_size_kernels.cu"
+        )
+    ],
+)
+
+
+def moe_align_block_size(
+    topk_ids: torch.Tensor, block_size: int, num_experts: int
+) -> Tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
+    sorted_ids = torch.empty(
+        (topk_ids.numel() + num_experts * (block_size - 1),),
+        dtype=torch.int32,
+        device=topk_ids.device,
+    )
+    expert_ids = torch.empty(
+        (topk_ids.numel() + num_experts,), dtype=torch.int32, device=topk_ids.device
+    )
+    sorted_ids.fill_(topk_ids.numel())
+    num_tokens_post_pad = torch.empty((1), dtype=torch.int32, device=topk_ids.device)
+    expert_off = torch.empty((num_experts), dtype=torch.int32, device=topk_ids.device)
+    expert_length = torch.empty(
+        (num_experts), dtype=torch.int32, device=topk_ids.device
+    )
+
+    ampere_fp8.ops.moe_align_block_size(
+        topk_ids,
+        num_experts,
+        block_size,
+        sorted_ids,
+        expert_ids,
+        num_tokens_post_pad,
+        expert_off,
+        expert_length,
+    )
+    return sorted_ids, expert_ids, num_tokens_post_pad, expert_off, expert_length
+
+
+def fused_moe(
+    hidden_states: torch.Tensor,
+    w1: torch.Tensor,
+    w2: torch.Tensor,
+    gating_output: torch.Tensor,
+    topk: int,
+    renormalize: bool,
+    inplace: bool = False,
+    override_config: Optional[Dict[str, Any]] = None,
+    use_fp8: bool = False,
+    w1_scale: Optional[torch.Tensor] = None,
+    w2_scale: Optional[torch.Tensor] = None,
+    a1_scale: Optional[torch.Tensor] = None,
+    a2_scale: Optional[torch.Tensor] = None,
+    routing_func: Callable = torch.topk,
+) -> torch.Tensor:
+    # Check constraints.
+    assert hidden_states.shape[0] == gating_output.shape[0], "Number of tokens mismatch"
+    assert hidden_states.shape[1] == w1.shape[2], "Hidden size mismatch"
+    assert gating_output.shape[1] == w1.shape[0], "Number of experts mismatch"
+    assert hidden_states.is_contiguous(), "Hidden_states must be contiguous"
+    assert w1.is_contiguous(), "Expert weights1 must be contiguous"
+    assert w2.is_contiguous(), "Expert weights2 must be contiguous"
+    assert hidden_states.dtype in [torch.float32, torch.float16, torch.bfloat16]
+    M, _ = hidden_states.shape
+    E, N, _ = w1.shape
+
+    if routing_func != torch.topk:
+        topk_weights, topk_ids = routing_func(gating_output, topk)
+
+    sorted_ids, expert_ids, num_tokens_post_pad, expert_off, expert_length = (
+        moe_align_block_size(topk_ids, 128, E)
+    )
+    
+    # assert torch.sum(expert_length) == M * topk, f"Number of tokens mismatch, {torch.sum(expert_length)} != {M * topk}"
+
+    intermediate_cache1 = torch.empty(
+        (M, topk_ids.shape[1], N),
+        device=hidden_states.device,
+        dtype=hidden_states.dtype,
+    )
+    intermediate_cache2 = torch.empty(
+        (M * topk_ids.shape[1], N // 2),
+        device=hidden_states.device,
+        dtype=hidden_states.dtype,
+    )
+    intermediate_cache3 = torch.empty(
+        (M, topk_ids.shape[1], w2.shape[1]),
+        device=hidden_states.device,
+        dtype=hidden_states.dtype,
+    )
+
+    compute_type = tl.bfloat16 if hidden_states.dtype == torch.bfloat16 else tl.float16
+
+    ops.silu_and_mul(intermediate_cache2, intermediate_cache1.view(-1, N))
+
+    if inplace:
+        return torch.sum(
+            intermediate_cache3.view(*intermediate_cache3.shape),
+            dim=1,
+            out=hidden_states,
+        )
+    return torch.sum(intermediate_cache3.view(*intermediate_cache3.shape), dim=1)
---
+++ vllm/model_executor/layers/fused_moe/execute.py
@@ -0,0 +1,147 @@
+import vllm
+import torch
+from vllm import _custom_ops as ops
+import argparse
+import json
+import sys
+import os
+import shutil
+
+import fused_moe
+import ampere_fp8_fused_moe_large_tokens
+
+
+def sparsemixer(scores, top_k, jitter_eps=0.01):
+    assert top_k == 2
+
+    ################ first expert ################
+
+    with torch.no_grad():
+        # compute mask for sparsity
+        mask_logits_threshold, max_ind = scores.max(dim=-1, keepdim=True)
+        factor = scores.abs().clamp(min=mask_logits_threshold)
+        mask_logits_threshold = ((mask_logits_threshold - scores) / factor) > (
+            2 * jitter_eps
+        )
+
+    # apply mask
+    masked_gates = scores.masked_fill(mask_logits_threshold, float("-inf"))
+    selected_experts = max_ind
+
+    # compute scores for gradients
+    masked_gates = torch.softmax(masked_gates, dim=-1)
+    multiplier_o = masked_gates.gather(dim=-1, index=selected_experts)
+
+    multiplier = multiplier_o
+
+    # masked out first expert
+    masked_scores = torch.scatter(
+        scores,
+        -1,
+        selected_experts,
+        float("-inf"),
+    )
+    with torch.no_grad():
+        # compute mask for sparsity
+        mask_logits_threshold, max_ind = masked_scores.max(dim=-1, keepdim=True)
+        factor = scores.abs().clamp(min=mask_logits_threshold)
+        mask_logits_threshold = ((mask_logits_threshold - scores) / factor) > (
+            2 * jitter_eps
+        )
+
+    # apply mask
+    masked_gates_top2 = masked_scores.masked_fill(mask_logits_threshold, float("-inf"))
+    selected_experts_top2 = max_ind
+    # compute scores for gradients
+    masked_gates_top2 = torch.softmax(masked_gates_top2, dim=-1)
+    multiplier_top2 = masked_gates_top2.gather(dim=-1, index=selected_experts_top2)
+
+    multiplier = torch.concat((multiplier, multiplier_top2), dim=-1)
+    selected_experts = torch.concat((selected_experts, selected_experts_top2), dim=-1)
+
+    return (
+        multiplier,
+        selected_experts,
+    )
+
+
+def moe_perf(
+    tokens=1024,
+    experts=8,
+    topk=2,
+    intermediate_size=14336,
+    hidden_size=4096,
+    config=None,
+    times=100,
+    use_fp8 = True
+):
+    torch.manual_seed(0)
+
+    hidden_state = torch.randn(tokens, hidden_size).cuda().bfloat16()
+
+    if use_fp8:
+        w1, ws_scale = ops.scaled_fp8_quant(
+            torch.randn(experts, intermediate_size * topk, hidden_size).cuda().half()
+        )
+        w2, w2s_scale = ops.scaled_fp8_quant(
+            torch.randn(experts, hidden_size, intermediate_size).cuda().half()
+        )
+        _, h_scale = ops.scaled_fp8_quant(hidden_state)
+        ws_scale = torch.ones(experts, dtype=ws_scale.dtype, device=ws_scale.device)
+        w2s_scale = torch.ones(experts, dtype=ws_scale.dtype, device=ws_scale.device)
+        fused_moe_f = ampere_fp8_fused_moe_large_tokens.fused_moe
+    else:
+        w1 = torch.randn(experts, intermediate_size * topk, hidden_size).cuda().bfloat16()
+        w2 = torch.randn(experts, hidden_size, intermediate_size).cuda().bfloat16()
+        h_scale = None
+        ws_scale = None
+        w2s_scale = None
+        fused_moe_f = fused_moe.fused_moe
+
+    gatew = torch.randn(hidden_size, experts).cuda().half()
+    gating_output = torch.matmul(hidden_state.half(), gatew).float()
+
+    all_time = 0.0
+    for j in range(10 + times):
+        start = torch.cuda.Event(enable_timing=True)
+        end = torch.cuda.Event(enable_timing=True)
+        start.record()
+
+        fused_moe_f(
+            hidden_states=hidden_state,
+            w1=w1,
+            w2=w2,
+            gating_output=gating_output,
+            topk=topk,
+            override_config=config,
+            renormalize=True,
+            inplace=True,
+            use_fp8=use_fp8,
+            w1_scale=ws_scale,
+            w2_scale=w2s_scale,
+            a1_scale=h_scale,
+            a2_scale=h_scale,
+            routing_func=sparsemixer
+        )
+
+        end.record()
+        torch.cuda.synchronize()
+        elapsed_time_ms = start.elapsed_time(end)
+
+        if j >= 10:
+            all_time += elapsed_time_ms
+
+    return all_time / times
+
+
+searchspace = [1] + list(range(0, 256, 32))[1:] + list(range(256, 4097, 256))
+#searchspace = [1, 4096]
+intermediate_size = 6400
+expert_num = 16
+
+for tk in searchspace:
+    print(
+        tk,
+        ",",
+        moe_perf(tokens=tk, experts=expert_num, intermediate_size=intermediate_size, use_fp8=True),
+    )
